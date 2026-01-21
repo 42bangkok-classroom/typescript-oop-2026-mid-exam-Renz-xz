06a68d831c61cf153972b4c617919ae0b87a001e
unknown <woranon17340@gmail.com>
2026-01-21 15:04:49 +0700

    exam

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -1,1 +1,20 @@
 // Write your code below
+const input = +process.argv[2]
+if (input>=0 && input<=49) {
+    console.log('F')
+}
+else if (input>=50 && input<=59) {
+    console.log('D')
+}
+else if (input>=60 && input<=69) {
+    console.log('C')
+}
+else if (input>=70 && input<=79) {
+    console.log('B')
+}
+else if (input>=80 && input<=100) {
+    console.log('A')
+}
+else {
+    console.log('Invalid input')
+}
--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -1,1 +1,15 @@
-// Write your code below
+const num1 = +process.argv[2]
+for (let i = 1; i === num1; i++) {
+    if(i%3===0 && i%5===0){
+        console.log('FizzBuzz')
+    }
+    else if(i%3===0){
+        console.log('Fizz')
+    }
+    else if(i%5===0){
+        console.log('Buzz')
+    }
+    else {
+        console.log(i)
+    }
+}
