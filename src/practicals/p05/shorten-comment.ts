58080701db60ffca02ec90e60ccb1afd6558659e
unknown <woranon17340@gmail.com>
2026-01-21 16:23:59 +0700

    exam

--- src/practicals/p05/shorten-comment.ts
+++ src/practicals/p05/shorten-comment.ts
@@ -1,6 +1,6 @@
 const comment = "TypeScript is a strongly typed programming language"
 
-function shortenComment(comment: string): string {
+export function shortenComment(comment: string): string {
   let words: string[] = comment.split(' ');
   let filteredwords: string[] = words.filter((word) => word.length>=5 && word.length<=10);
   console.log(filteredwords);
